63839e1687b244be2ed41b19bc603bbb0c91aa15
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-10-12 21:19:27 -0500

    trying to update main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 //TODO: Search History
 //TODO: Make Modals Work
 
-$(document).foundation();
-
+var popup = new Foundation.Reveal($('#exampleModal1'));
+// var aboutModal = document.querySelector(".list-item-2")
+// var modalbg = document.querySelector(".modal-bg")
+// var closeBtn = document.getElementById("close-button")
+var pastSearches = []
 var featuredArticles = [
     document.querySelector(".article-0"),
     document.querySelector(".article-1"),
@@ -11,15 +14,16 @@ var featuredArticles = [
 ]
 
 var priceForm = document.querySelector(".price-form");
-var exchangeForm = document.querySelector(".exchange-rates-form");
-var modalText = document.querySelector(".modal-text");
-var modalTrigger = document.querySelector(".modal-trigger");
-
+var exchangeForm = document.querySelector(".exchange-rates-form")
 
 document.addEventListener("click", inputFocusHandler);
 priceForm.addEventListener("submit", priceFormSubmitHandler);
 exchangeForm.addEventListener("submit", exchangeFormSubmitHandler);
 
+$(window).ready(function() {
+    $('#history-div').append(`<h1 class="search-history">SEARCH HISTORY</h1>`)
+    addLocalStorageToScreen()
+})
 getFeaturedNews();
 // Gets featured news.
 function getFeaturedNews() {
@@ -27,9 +31,9 @@ function getFeaturedNews() {
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                // console.log(data);
                 for (var i = 0; i < featuredArticles.length; i++) {
                     featuredArticles[i].setAttribute("href", data.results[i].article_url);
-                    featuredArticles[i].setAttribute("target", "_blank");
                     if (data.results[i].image_url) {
                         featuredArticles[i].querySelector(".featured-image").style.backgroundImage = "url('" + data.results[i].image_url + "')";
                     } else {
@@ -40,10 +44,8 @@ function getFeaturedNews() {
                 }
             });
         } else {
-            modal("Unexpected error.")
+            displayModal('ALERT!:  Bad API Call')
         }
-    }).catch(function () {
-        modal("Unable to connect to Polygon.");
     });
 }
 
@@ -60,7 +62,7 @@ function priceFormSubmitHandler(event) {
     if (priceInput) {
         stockApi(priceInput)
     } else {
-        modal("Please enter a stock or cryptocurrency.");
+        displayModal("ALERT!:  Please enter a stock or cryptocurrency.");
     }
 }
 
@@ -74,84 +76,72 @@ function exchangeFormSubmitHandler(event) {
         if (parseFloat(volumeInput)) {
             getInfo(baseInput, exchangeInput, volumeInput);
         } else {
-            modal("Please enter a number for Conversion Amount.")
+            displayModal("ALERT!:  Please enter a number for Conversion Amount")
         }
     } else {
-        modal("Please fill all inputs in the form.")
+        displayModal("ALERT!:  Please fill all inputs in the form")
     }
 }
 
 // Gets stock prices.
 function stockApi(ticker) {
-    let test = false;
-    let counter = 1;
-    while(!test){
-        let testdate = moment().subtract(counter,"days").format('dddd');
-        if(testdate === "Sunday" || testdate === "Saturday"){
-            counter++
-        } else{
-            test = true;
-        }
-    }
-    let date = moment().subtract(counter, "days").format('YYYY-MM-DD');
-    let apiUrl = `https://api.polygon.io/v1/open-close/${ticker}/${date}?adjusted=true&apiKey=bOZCwGtAFurvAO_gqOPxaOvqmw8ALJWg`;
+    let apiUrl = "https://api.polygon.io/v1/open-close/" + ticker + "/" + getPreviousDate() + "?adjusted=true&apiKey=rSbWvupXYcUkBP6mLKFppfHMRHKEmL1p";
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                document.querySelector(".price-date").textContent = "(" + date + ")";
-                priceForm.querySelector(".open").textContent = "$" + data.open.toFixed(2);
+                // console.log(data);
                 priceForm.querySelector(".close").textContent = "$" + data.close.toFixed(2);
                 priceForm.querySelector(".high").textContent = "$" + data.high.toFixed(2);
                 priceForm.querySelector(".low").textContent = "$" + data.low.toFixed(2);
+                priceForm.querySelector(".volume").textContent = data.volume.toFixed(2);
+                console.log(data)
+                addToLocalStorage(data)
             });
         } else if (response.status === 404) {
             getCryptoPrice(ticker);
         } else {
-            modal("Unexpected error.")
+            displayModal("ALERT!:  Unexpected Error")
         }
     }).catch(function () {
-        modal("Unable to connect to Polygon.");
+        displayModal("ALERT!:  Unable to connect to Polygon");
     });
 }
 
 // Gets the Crypto Price.
 function getCryptoPrice(ticker) {
-    let date = moment().subtract(1, "days").format("YYYY-MM-DD");
-    let apiUrl = `https://api.polygon.io/v1/open-close/crypto/${ticker}/USD/${date}?adjusted=true&apiKey=bOZCwGtAFurvAO_gqOPxaOvqmw8ALJWg`
+    let apiUrl = "https://api.polygon.io/v1/open-close/crypto/" + ticker + "/USD/" + getPreviousDate() + "?adjusted=true&apiKey=rSbWvupXYcUkBP6mLKFppfHMRHKEmL1p"
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 if (data.close > 0) {
-                    document.querySelector(".price-date").textContent = "(" + date + ")";
-                    priceForm.querySelector(".open").textContent = "$" + data.open.toFixed(2);
                     priceForm.querySelector(".close").textContent = "$" + data.close.toFixed(2);
                     priceForm.querySelector(".high").textContent = "--";
                     priceForm.querySelector(".low").textContent = "--";
+                    priceForm.querySelector(".volume").textContent = "--";
                 } else {
-                    modal("The Stock/Cryptocurrency was not found.");
+                    displayModal("ALERT!:  Not found");
                 } 
             });
         } else {
-            modal("Unexpected error.");
+            displayModal("ALERT!:  Unexpected Error");
         }
     }).catch(function () {
-        modal("Unable to connect to Polygon.");
+        displayModal("ALERT!:  Unable to connect to Polygon");
     });
 }
 
 function getInfo(coin, exchange, vol) {
-    let date = moment().format("YYYY-MM-DD");
     let apiUrl = "https://rest-sandbox.coinapi.io/v1/exchangerate/" + coin + "?apikey=09391D71-51BB-4594-A7C1-9AE2C45D8099";
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                console.log(data)
                 if (data.rates.length === 0) {
-                    modal("Did not recognize Cryptocurrency (Base).");
+                    displayModal("ALERT!:  Did not regonise Cryptocurrency (Base)");
                 } else {
                     var updated = false;
                     for (let i = 0; i < data.rates.length; i++) {
                         if (exchange === data.rates[i].asset_id_quote) {
-                            document.querySelector(".exchange-date").textContent = date;
                             document.querySelector(".exchange-rates-flex-container p").className = "border-visable";
                             document.querySelector(".base-amount").textContent = vol + " ";
                             document.querySelector(".base").textContent = coin + " = ";
@@ -161,21 +151,24 @@ function getInfo(coin, exchange, vol) {
                         }
                     }
                     if (!updated) {
-                        modal("Did not recognize Cryptocurrency (Exchange).")
+                        displayModal("ALERT!:  Did not regonise Cryptocurrency (Exchange)")
                     }
                 }
             });
         } else {
-            modal("Unexpected error.")
+            displayModal("ALERT!:  Unexpected error")
         }
     }).catch(function () {
-        modal("Unable to connect to Sandbox.");
+        displayModal("ALERT!:  Unable to connect to Sandbox");
     });
 }
 
-function modal(text) {
-    modalText.textContent = text;
-    modalTrigger.click();
+function getPreviousDate() {
+    var d = new Date();
+    var date = d.getFullYear() + "-";
+    date += (d.getMonth() + 1) + "-";
+    date += (d.getDate() - 1);
+    return date;
 }
 //----------------------------------------------------------------------------------------------------------
 
@@ -189,160 +182,132 @@ function modal(text) {
 //     modalbg.classList.remove("bg-active");
 // });
 
-//temporary to get history working
-// $("#previous-close-price-form").submit(function (event) {
-//     var inputTest = $(".input-group-field").val().toUpperCase().trim()
-//     console.log(inputTest)
-//     event.preventDefault()
-//     stockApi(inputTest) 
-// })
-
-// $( ".card-list" ).click(function(event) {
-//     console.log("clicked: " + event.target);
-// });
+$( ".card-list" ).click(function(event) {
+    console.log("clicked: " + event.target);
+});
   
 
-// function addHistory(tickerObj, count) {
-//     var html = $(`<div id=${count} class="product-card">
-//     <div class="product-card-thumbnail">
-//     <h1>${tickerObj.ticker}</h1>
-//     </div>
-//     <ul class="card-list">
-//     <li>
-//         <h2 class="product-card-title">Date: ${tickerObj.date}</h2>
-//     </li>
-//     <li>
-//         <h2 class="product-card-title">Close: ${tickerObj.close}</h2>
-//     </li>
-//     <li>
-//         <h2 class="product-card-title">High: ${tickerObj.high}</h2>
-//     </li>
-//     <li>
-//         <h2 class="product-card-title">Low: ${tickerObj.low}</h2>
-//     </li>
-//     <li>
-//         <h2 class="product-card-title">Open: ${tickerObj.open}</h2>
-//     </li>
-//     </ul>
-//     `)
+function addHistory(tickerObj, count) {
+    var html = $(`<div id=${count} class="product-card">
+    <div class="product-card-thumbnail">
+    <h1>${tickerObj.ticker}</h1>
+    </div>
+    <ul class="card-list">
+    <li>
+        <h2 class="product-card-title">Date: ${tickerObj.date}</h2>
+    </li>
+    <li>
+        <h2 class="product-card-title">Close: ${tickerObj.close}</h2>
+    </li>
+    <li>
+        <h2 class="product-card-title">High: ${tickerObj.high}</h2>
+    </li>
+    <li>
+        <h2 class="product-card-title">Low: ${tickerObj.low}</h2>
+    </li>
+    <li>
+        <h2 class="product-card-title">Open: ${tickerObj.open}</h2>
+    </li>
+    </ul>
+    `)
 
-//     $('#history-div').append(html)
-// }
+    $('#history-div').append(html)
+}
 
-// function addToLocalStorage(data) {
-//     var today = new Date().toISOString().slice(0, 10)
-//     var dataObj = {}
-//     dataObj['date'] = today
-//     dataObj['ticker'] = data.results[0].T
-//     dataObj['close'] = data.results[0].c
-//     dataObj['high'] = data.results[0].h
-//     dataObj['low'] = data.results[0].l
-//     dataObj['open'] = data.results[0].o
+function addToLocalStorage(data) {
+    console.log(data)
+    var today = new Date().toISOString().slice(0, 10)
+    var dataObj = {}
+    dataObj['date'] = today
+    dataObj['ticker'] = data.symbol
+    dataObj['close'] = data.close
+    dataObj['high'] = data.high
+    dataObj['low'] = data.low
+    dataObj['open'] = data.open
+    console.log('in addToLocalStorage function', dataObj.ticker)
+    if (!pastSearches.includes(dataObj.ticker) && pastSearches.length < 7) {
+        pastSearches.push(dataObj)
+        localStorage.setItem('searchHistroy', JSON.stringify(pastSearches))
 
-//     console.log('in addToLocalStorage function', dataObj.ticker)
-//     if (!pastSearches.includes(data.ticker) && pastSearches.length < 2) {
-//         pastSearches.push(dataObj)
-//         // $('#history-div').append(html)
-//         localStorage.setItem('searchHistroy', JSON.stringify(pastSearches))
+        addLocalStorageToScreen()
+    } else if (pastSearches.length >= 7) {
+        console.log(pastSearches.shift())
+        pastSearches.push(dataObj)
+        console.log('in elseif', pastSearches)
+        localStorage.setItem('searchHistroy', JSON.stringify(pastSearches))
 
-//         addLocalStorageToScreen()
-//     } else if (pastSearches.length >= 2) {
-//         console.log(pastSearches.shift())
-//         pastSearches.push(dataObj)
-//         console.log('in elseif', pastSearches)
-//         localStorage.setItem('searchHistroy', JSON.stringify(pastSearches))
+        addLocalStorageToScreen()
+    }
+}
 
-//         addLocalStorageToScreen()
-//     }
-// }
+function addLocalStorageToScreen() {
+    console.log('in addLocalStorageToScreen function')
+    $('#history-div').empty()
+    $('#history-div').append(`<h1 class="search-history">SEARCH HISTORY</h1>`)
+    var searchedStock = {}
+    var count = 0
+    if (localStorage.getItem('searchHistroy')) {
+        pastSearches = JSON.parse(localStorage.getItem('searchHistroy'))
+        for (const element of pastSearches) {            
+            $.each(pastSearches, function(i, val) {
+                searchedStock[i] = val
+            })
+            console.log(element);
+            addHistory(element, count)
+            count++
+        }
+        console.log(searchedStock);
+    }
+}
+// function formSubmitHandler(event) {
+//     event.preventDefault();
+//     var coinType = coinTypeInput.textContent().toUpperCase().trim();
+//     var convertType = convertTypeInput.value.toUpperCase().trim();
+//     var volume = parseFloat(volumeInput.value);
 
-// function addLocalStorageToScreen() {
-//     console.log('in addLocalStorageToScreen function')
-//     $('#history-div').empty()
-//     $('#history-div').append(`<h1 class="search-history">SEARCH HISTORY</h1>`)
-//     var searchedStock = {}
-//     var count = 0
-//     if (localStorage.getItem('searchHistroy')) {
-//         pastSearches = JSON.parse(localStorage.getItem('searchHistroy'))
-//         for (const element of pastSearches) {            
-//             $.each(pastSearches, function(i, val) {
-//                 searchedStock[i] = val
-//             })
-//             console.log(element);
-//             addHistory(element, count)
-//             count++
+//     if (coinType) {
+//         if (convertType) {
+//             if (typeof (volume) === "number" && volume > 0) {
+//                 getInfo(coinType, convertType, volume);
+//             } else {
+//                 // alert("Please enter a valid amount!");
+//                 displayModal("ALERT!:  Please enter a valid amount!")                  
+//             }
+//         } else {
+//             //alert("Please enter a valid stock or cryptocurrecty converstion!")
+//             displayModal("ALERT!:  Please enter a valid stock or cryptocurrecty converstion!")            
 //         }
-//         console.log(searchedStock);
+//     } else {
+//         //alert("Please enter a valid stock or cryptocurrency!");
+//         displayModal("ALERT!:  Please enter a valid stock or cryptocurrency!")
 //     }
 // }
-// // function formSubmitHandler(event) {
-// //     event.preventDefault();
-// //     var coinType = coinTypeInput.textContent().toUpperCase().trim();
-// //     var convertType = convertTypeInput.value.toUpperCase().trim();
-// //     var volume = parseFloat(volumeInput.value);
+//for error handling
+function displayModal(text) {
+    var alertColor = "rgba(215, 54, 29, 1)";
 
-// //     if (coinType) {
-// //         if (convertType) {
-// //             if (typeof (volume) === "number" && volume > 0) {
-// //                 getInfo(coinType, convertType, volume);
-// //             } else {
-// //                 // alert("Please enter a valid amount!");
-// //                 displayModal("Please enter a valid amount!")                  
-// //             }
-// //         } else {
-// //             //alert("Please enter a valid stock or cryptocurrecty converstion!")
-// //             displayModal("Please enter a valid stock or cryptocurrecty converstion!")            
-// //         }
-// //     } else {
-// //         //alert("Please enter a valid stock or cryptocurrency!");
-// //         displayModal("Please enter a valid stock or cryptocurrency!")
-// //     }
-// // }
-// //for error handling
-// function displayModal(text) {
-//     var alertColor = "rgba(215, 54, 29, 1)";
+    $('#alertBox').css("background-color", alertColor);
+    $('#alert').text(` ${text}`)
+    popup.open();
+}
 
-//     $('#warningBox').hide()
-//     $('#successBox').hide()
-//     $('#alertBox').css("background-color", alertColor);
-//     $('#alert').text(`     ${text}`)
-//     popup.open();
-// }
+// getTickerNews("RCAT")
+//passes in ticker
+//articles for specific tickers
+// let getTickerNews = function (ticker) {
+//     let apiUrl = `https://api.polygon.io/v2/reference/news?limit=10&order=descending&sort=published_utc&ticker=${ticker}&apiKey=bOZCwGtAFurvAO_gqOPxaOvqmw8ALJWg`;
+//     fetch(apiUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 response.json().then(function (data) {
+//                     // stock based articles;
 
-// // function displayModal(text) {
-// //     var new_rgba_str = "rgba(215, 54, 29, 1)";
+//                 })
+//             }
+//         })
+//         .catch(function (error) {
 
-// //     $('#warningBox').hide()
-// //     $('#successBox').hide()
-// //     $('#alertBox').css("background-color", new_rgba_str);
-// //     $('#alert').text(text)
-// //     popup.open();
-// // }
-
-// //                 })
-// //             } else {
-// //                 displayModal('Bad API Call')
-// //             }
-// //         })
-// // }
-
-// // getTickerNews("RCAT")
-// //passes in ticker
-// //articles for specific tickers
-// // let getTickerNews = function (ticker) {
-// //     let apiUrl = `https://api.polygon.io/v2/reference/news?limit=10&order=descending&sort=published_utc&ticker=${ticker}&apiKey=bOZCwGtAFurvAO_gqOPxaOvqmw8ALJWg`;
-// //     fetch(apiUrl)
-// //         .then(function (response) {
-// //             if (response.ok) {
-// //                 response.json().then(function (data) {
-// //                     // stock based articles;
-
-// //                 })
-// //             }
-// //         })
-// //         .catch(function (error) {
-
-// //         })
-// //}
+//         })
+//}
 
 
